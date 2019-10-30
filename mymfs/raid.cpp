d1634afcbf7fdb3c3f9fe54d266c01ff8c2bb2c9
Bruno Marini <bruno.marini1@gmail.com>
2019-10-30 14:26:13 -0300

    .temp file with physical units [+]

--- mymfs/raid.cpp
+++ mymfs/raid.cpp
@@ -1,5 +1,74 @@
+// Cabeçalhos
 #include "pch.h"
-#include "raid.h"
-#include "unidade.h"
+#include "utils.h"
 
+//#include "unidade.h"
 
+// Bibliotecas
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <string>
+#include <filesystem>
+
+namespace fsys = std::filesystem;
+using namespace std;
+
+bool arquivoExiste (string arquivo) {
+	if (arquivo.empty())
+		return false;
+	return fsys::exists(arquivo);
+}
+
+// Adiciona múltiplas unidades físicas ao .temp
+bool adicionarUnidadesFisicas (string caminhoArquivo, vector <string> unidadesFisicas) {
+	try
+	{
+		ofstream arquivo(caminhoArquivo, ios_base::app | ios_base::out);
+
+		string linha = "";
+		for (int i = 0; i < unidadesFisicas.size(); i++)
+			linha += unidadesFisicas[i] + "\n";
+
+		arquivo << linha;
+		arquivo.close();
+	}
+	catch (const std::exception & ex)
+	{
+		cout << ex.what() << endl;
+		return false;
+	}
+	
+	return true;
+}
+
+// TODO: implementar a possibilidade de adicionar uma nova unidade física depois do Raid configurado
+// Configurar Raid
+bool configRaid(string unidadeLogica, vector <string> unidadesFisicas, string caminhoBase, string barra) {
+	string caminhoPasta = caminhoBase + barra + "temp";
+	string caminhoArquivo = caminhoPasta + barra + "mymfs.temp";
+
+	string unidadesFisicasStr;
+	for (int i = 0; i < unidadesFisicas.size(); i++)
+		unidadesFisicasStr += unidadesFisicas[i] + ' ';
+
+	if (!fsys::exists(caminhoPasta))
+		fsys::create_directory(caminhoPasta);
+
+	if (!fsys::exists(caminhoArquivo)) {
+		if (adicionarUnidadesFisicas(caminhoArquivo, unidadesFisicas)) {
+			cout << "O MyMFS configurado com sucesso!" << endl;
+			cout << "- Unidade logica: " + unidadeLogica << endl;
+			cout << "- Unidades fisicas: " + unidadesFisicasStr << endl;
+			return true;
+		}
+
+		return false;
+	}
+	else {
+		cout << "Ja existe um MyMFS com estas configuracoes" << endl;
+		cout << "- Unidade logica: " + unidadeLogica << endl;
+		cout << "- Unidades fisicas: " + unidadesFisicasStr << endl;
+		return false;
+	}
+}
--- mymfs/raid.h
+++ mymfs/raid.h
@@ -1,4 +1,10 @@
-#ifndef RAID.H
+#ifndef RAID_H
+#define RAID_H
 
+#include <string>
+#include <filesystem>
+using namespace std;
+
+bool configRaid (string unidadeLogica, vector <string> unidadesFisicas, string caminhoBase, string barra);
 
 #endif //RAID.H
