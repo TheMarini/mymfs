6f148e08c73efa241a98d225a312248a1af37601
Bruno Marini <bruno.marini1@gmail.com>
2019-10-30 14:26:31 -0300

    Utils [+] {println}

---
+++ mymfs/utils.cpp
@@ -0,0 +1,13 @@
+// Cabeçalhos
+#include "pch.h"
+
+// Bibliotecas
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+using namespace std;
+
+void println (string texto) {
+	cout << texto << endl;
+}
